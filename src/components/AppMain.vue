1b758ab9fab2d4ca9e4855dc9c449104ccf58c9c
silvialep <silvia.lepore_sl@libero.it>
2023-03-28 17:46:12 +0200

    feat: printed images in page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,8 @@ import { store } from "../store.js";
 
 import axios from 'axios';
 
+import CardItem from "./CardItem.vue";
+
 
 export default {
 
@@ -19,12 +21,16 @@ export default {
     },
 
     components: {
-
+        CardItem,
     },
 
     created() {
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
-            console.log(res.data);
+            // console.log(res.data.data[0].card_images[0].image_url);
+            console.log(res.data.data);
+
+            this.store.cards = res.data.data;
+
         })
     },
 
@@ -36,11 +42,20 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div id="main-container">
+        <CardItem v-for="card in store.cards" :card="card"></CardItem>
 
     </div>
 </template>
 
 <style lang="scss" scoped>
+
+@use "../scss/variables.scss" as *;
+
+
+#main-container {
+    @include mainContainer();
+
+}
 
 </style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,51 @@
+<script>
+
+
+
+export default {
+
+    name: "CardItem",
+
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+
+        card: Object,
+
+    },
+
+
+
+}
+
+
+</script>
+
+<template>
+    <div class="single-card">
+
+        <img :src="card.card_images[0].image_url" alt="">
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use "../scss/variables.scss" as *;
+
+.single-card {
+    width: calc(100% / 6);
+    height: 300px;
+
+    img {
+        height: 100%;
+    }
+}
+
+
+
+</style>
