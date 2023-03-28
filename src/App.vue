4b949823d46e62d90080ba28cac83f3944d2e443
silvialep <silvia.lepore_sl@libero.it>
2023-03-28 14:52:56 +0200

    feat: added api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <script>
+
+import AppMain from './components/AppMain.vue';
+
+export default {
+
+  // name: -nomecomponent -,
+
+  data() {
+    return {
+
+
+
+    }
+  },
+
+  
+
+  components: {
+    AppMain,
+
+  },
+
+
+
+}
+
+
 </script>
 
 <template>
+
+  <AppMain></AppMain>
   
 </template>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+
+import { store } from "../store.js";
+
+import axios from 'axios';
+
+
+export default {
+
+    name: "AppMain",
+
+    data() {
+        return {
+
+
+            store,
+
+        }
+    },
+
+    components: {
+
+    },
+
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+            console.log(res.data);
+        })
+    },
+
+
+
+}
+
+
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
